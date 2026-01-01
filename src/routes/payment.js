e7afe385d891ba46d62497c2230983afa4ebdec5
Dans3101 <sanwhat898@gmail.com>
2026-01-01 20:44:37 +0300

    Update payment.js

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,1 +1,12 @@
-1
+import express from 'express';
+import {
+  activateBot,
+  payPerformanceFee
+} from '../controllers/paymentController.js';
+
+const router = express.Router();
+
+router.post('/activate', activateBot);
+router.post('/performance-fee', payPerformanceFee);
+
+export default router;
