581b63c07cd52d26001a64dfe325015644e68fea
Dans3101 <sanwhat898@gmail.com>
2026-01-01 15:35:04 +0300

    Update riskManager.js

--- src/bot/riskManager.js
+++ src/bot/riskManager.js
@@ -1,1 +1,26 @@
-1
+import { SETTINGS } from '../config/deriv.js';
+
+export function calculateStake(balance) {
+  return balance * SETTINGS.RISK_PERCENT;
+}
+
+export function checkLimits(user) {
+  const profitTarget =
+    user.startBalance * (1 + SETTINGS.STOP_PROFIT_MULTIPLIER);
+  const lossLimit =
+    user.startBalance * (1 - SETTINGS.STOP_LOSS_PERCENT);
+
+  if (user.currentBalance >= profitTarget) {
+    return 'PROFIT_TARGET_REACHED';
+  }
+
+  if (user.currentBalance <= lossLimit) {
+    return 'LOSS_LIMIT_REACHED';
+  }
+
+  if (user.tradesToday >= SETTINGS.MAX_TRADES_PER_DAY) {
+    return 'MAX_TRADES_REACHED';
+  }
+
+  return 'OK';
+}
