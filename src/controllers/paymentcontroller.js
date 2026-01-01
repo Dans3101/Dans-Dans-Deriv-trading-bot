b2afd8b434e0f6ee73a6cf9332731c6ccf8b77fe
Dans3101 <sanwhat898@gmail.com>
2026-01-01 20:40:46 +0300

    Update paymentcontroller.js

--- src/controllers/paymentcontroller.js
+++ src/controllers/paymentcontroller.js
@@ -1,1 +1,22 @@
-1
+import { usersDB } from '../db/memoryStore.js';
+
+export function activateBot(req, res) {
+  const { email } = req.body;
+
+  if (!usersDB[email]) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  usersDB[email].activationPaid = true;
+  usersDB[email].activated = true;
+
+  res.json({ message: 'Bot activated' });
+}
+
+export function payPerformanceFee(req, res) {
+  const { email } = req.body;
+
+  usersDB[email].performanceFeePaid = true;
+
+  res.json({ message: 'Performance fee cleared' });
+}
