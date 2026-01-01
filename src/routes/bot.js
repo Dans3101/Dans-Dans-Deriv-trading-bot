820ce38eb6136505f243c5c6ae6cc346c2c51a9c
Dans3101 <sanwhat898@gmail.com>
2026-01-01 20:38:54 +0300

    Update bot.js

--- src/routes/bot.js
+++ src/routes/bot.js
@@ -1,1 +1,9 @@
-1
+import express from 'express';
+import { startBot, stopBot } from '../controllers/botController.js';
+
+const router = express.Router();
+
+router.post('/start', startBot);
+router.post('/stop', stopBot);
+
+export default router;
