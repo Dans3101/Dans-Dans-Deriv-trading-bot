792a135f5924a096bdb2efc814fa3f67a1c0a2ce
Dans3101 <sanwhat898@gmail.com>
2026-01-01 15:33:04 +0300

    Update DerivBot.js

--- src/bot/DerivBot.js
+++ src/bot/DerivBot.js
@@ -1,1 +1,9 @@
-1
+export const DERIV_WS = (appId) =>
+  `wss://ws.derivws.com/websockets/v3?app_id=${appId}`;
+
+export const SETTINGS = {
+  MAX_TRADES_PER_DAY: 10,
+  RISK_PERCENT: 0.03,
+  STOP_PROFIT_MULTIPLIER: 2.0,
+  STOP_LOSS_PERCENT: 0.30,
+};
