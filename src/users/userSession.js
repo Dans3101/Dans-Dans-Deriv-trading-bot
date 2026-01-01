1da9e407fe8d10c6a9d2a6af3e4318ed56a4ea1d
Dans3101 <sanwhat898@gmail.com>
2026-01-01 15:34:16 +0300

    Update userSession.js

--- src/users/userSession.js
+++ src/users/userSession.js
@@ -1,1 +1,14 @@
-1
+export class UserSession {
+  constructor({ userId, apiToken, market }) {
+    this.userId = userId;
+    this.apiToken = apiToken;
+    this.market = market;
+
+    this.ws = null;
+    this.startBalance = 0;
+    this.currentBalance = 0;
+    this.tradesToday = 0;
+    this.active = false;
+    this.inTrade = false;
+  }
+}
