e58562fb501bf8a2956882c431cff9da613ef19e
Dans3101 <sanwhat898@gmail.com>
2026-01-01 15:36:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-1
+import 'dotenv/config';
+import { UserSession } from './users/userSession.js';
+import { DerivBot } from './bot/DerivBot.js';
+
+const users = [
+  {
+    userId: 'user_001',
+    apiToken: 'PUT_DERIV_TOKEN_HERE',
+    market: 'R_75'
+  }
+];
+
+users.forEach(userData => {
+  const session = new UserSession(userData);
+  const bot = new DerivBot(session);
+  bot.connect();
+});
