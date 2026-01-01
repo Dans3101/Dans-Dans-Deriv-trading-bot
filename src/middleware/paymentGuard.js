b1b5f5cd7581eae2761437ec1d628612938e9e00
Dans3101 <sanwhat898@gmail.com>
2026-01-01 20:07:41 +0300

    Update paymentGuard.js

--- src/middleware/paymentGuard.js
+++ src/middleware/paymentGuard.js
@@ -1,1 +1,14 @@
-1
+import { calculatePerformanceFee } from '../payments/fees.js';
+
+export function canTrade(user) {
+  const fee = calculatePerformanceFee(
+    user.startBalance,
+    user.maxBalance
+  );
+
+  if (fee > 0 && !user.performanceFeePaid) {
+    return false;
+  }
+
+  return true;
+}
