4acbed4e88493a8cf11abe52867f461e97059cf9
Dans3101 <sanwhat898@gmail.com>
2026-01-01 21:11:31 +0300

    Update adminController.js

--- src/admin/adminController.js
+++ src/admin/adminController.js
@@ -1,1 +1,26 @@
-1
+import { usersDB, bots } from '../db/memoryStore.js';
+
+export function getAllUsers(req, res) {
+  res.json(usersDB);
+}
+
+export function stopUserBot(req, res) {
+  const { email } = req.body;
+
+  if (bots[email]) {
+    bots[email].user.ws.close();
+    delete bots[email];
+  }
+
+  res.json({ message: `Bot stopped for ${email}` });
+}
+
+export function clearPerformanceFee(req, res) {
+  const { email } = req.body;
+
+  if (usersDB[email]) {
+    usersDB[email].performanceFeePaid = true;
+  }
+
+  res.json({ message: 'Performance fee cleared' });
+}
