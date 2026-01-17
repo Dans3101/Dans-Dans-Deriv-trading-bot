e1dcc737b9400c185392297a68304d37737ba5c4
Dans3101 <sanwhat898@gmail.com>
2026-01-17 16:56:28 +0300

    Update telegramAdmin.js

--- src/notifications/telegramAdmin.js
+++ src/notifications/telegramAdmin.js
@@ -2,7 +2,7 @@
 import TelegramBot from 'node-telegram-bot-api';
 
 const TELEGRAM_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const TELEGRAM_CHAT_ID = process.env.TELEGRAM_ADMIN_CHAT_ID;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 let bot = null;
 
