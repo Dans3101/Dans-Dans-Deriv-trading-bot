4e32e6d88ef49b904df96e202579ec6e397a4a9f
Dans3101 <sanwhat898@gmail.com>
2026-01-20 10:18:19 +0300

    Update userSession.js

--- src/users/userSession.js
+++ src/users/userSession.js
@@ -1,8 +1,9 @@
 export class UserSession {
-  constructor({ userId, apiToken, market }) {
+  constructor({ userId, apiToken, market, accountType = 'demo' }) {
     this.userId = userId;
     this.apiToken = apiToken;
     this.market = market;
+    this.accountType = accountType; // "demo" or "real"
 
     this.ws = null;
     this.startBalance = 0;
@@ -10,5 +11,8 @@ export class UserSession {
     this.tradesToday = 0;
     this.active = false;
     this.inTrade = false;
+
+    // Real accounts start as unpaid
+    this.performanceFeePaid = accountType === 'demo';
   }
 }
